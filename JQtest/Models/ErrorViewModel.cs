055ee5ef647cfefb3198a78da0d753470e1a522f
chris <kejun508@gmail.com>
2025-04-10 09:19:16 +0800

    加入專案檔案。

---
+++ JQtest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JQtest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
